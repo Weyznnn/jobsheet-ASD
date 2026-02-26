3f9d0eb79c765b42990469830325078f68df5ed5
Weyznnn <rafaninoadiyatma0989@gmail.com>
2026-02-26 10:43:10 +0700

    Revisi2

--- jobsheet3/Mahasiswa23.java
+++ jobsheet3/Mahasiswa23.java
@@ -1,6 +1,8 @@
 package jobsheet3;
 
 public class Mahasiswa23 {
-    public String nim, nama, kelas;
+    public String nim;
+    public String nama;
+    public String kelas;
     public float ipk;
 }
