c2e855859a2f6d6e8f3cd577143e1ea64739e3d8
Weyznnn <rafaninoadiyatma0989@gmail.com>
2026-02-26 12:02:40 +0700

    Percobaan 3

---
+++ jobsheet3/MatakKuliah23.java
@@ -0,0 +1,14 @@
+package jobsheet3;
+public class MatakKuliah23 {
+    public String kode;
+    public String nama;
+    public int sks; 
+    public int jumlahJam;
+
+    public MatakKuliah23(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
