57fa562d92493f4e0d5afbbe67efd2ad1a94abfe
Weyznnn <rafaninoadiyatma0989@gmail.com>
2026-05-08 09:15:31 +0700

    Percobaan2

--- Jobsheet11/SLLMain22.java
+++ Jobsheet11/SLLMain22.java
@@ -13,67 +13,95 @@ public class SLLMain22 {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int pilihUtama, pilihTambah;
         SingleLinkedList22 sll = new SingleLinkedList22();
-        // Mahasiswa22 mhs1 = new Mahasiswa22("24212200", "Alvaro", "1A", 4.0);
-        // Mahasiswa22 mhs2 = new Mahasiswa22("23212201", "Bimon", "2B", 3.8);
-        // Mahasiswa22 mhs3 = new Mahasiswa22("22212202", "Cintia", "3C", 3.5);
-        // Mahasiswa22 mhs4 = new Mahasiswa22("21212203", "Dirga", "4D", 3.6);
-        do {
-            System.out.println("Menu: ");
-            System.out.println("1. Tambah Data");
-            System.out.println("2. Tampilkan Data");
-            System.out.println("0. Keluar");
-            System.out.print("Pilih Menu: ");
-            pilihUtama = sc.nextInt();
-            switch (pilihUtama) {
-                case 1:
-                    System.out.println("Tambah Data Mahasiswa:");
-                    System.out.print("Nim: ");
-                    String nim = sc.next();
-                    System.out.print("Nama: ");
-                    String nama = sc.next();
-                    System.out.print("Kelas: ");
-                    String kelas = sc.next();
-                    System.out.print("IPK: ");
-                    double ipk = sc.nextDouble();
-                    Mahasiswa22 mhs = new Mahasiswa22(nim, nama, kelas, ipk);
+        // int pilihUtama, pilihTambah;
 
-                    menuTambahData();
-                    pilihTambah = sc.nextInt();
+        // Data setup
+        Mahasiswa22 mhs1 = new Mahasiswa22("24212200", "Alvaro", "1A", 4.0);
+        Mahasiswa22 mhs2 = new Mahasiswa22("23212201", "Bimon", "2B", 3.8);
+        Mahasiswa22 mhs3 = new Mahasiswa22("22212202", "Cintia", "3C", 3.5);
+        Mahasiswa22 mhs4 = new Mahasiswa22("21212203", "Dirga", "4D", 3.6);
 
-                    switch (pilihTambah) {
-                        case 1:
-                            sll.addFirst(mhs);
-                            break;
-                        case 2:
-                            sll.addLast(mhs);
-                            break;
-                        case 3:
-                            System.out.print("Masukkan Nama Data yang Akan Dimasukkan: ");
-                            String key = sc.next();
-                            sll.insertAfter(key, mhs);
-                            break;
-                        case 4:
-                            System.out.print("Masukkan Index Data yang Akan Dimasukkan: ");
-                            int index = sc.nextInt();
-                            sll.insertAt(index, mhs);
-                            break;
-                        default:
-                            System.out.println("Pilihan Tidak Tersedia");
-                        }
-                        sll.print();
-                    break;
-                case 2:
-                    sll.print();
-                    break;
-                case 0:
-                    System.out.println("Keluar");
-                    break;
-                default:
-                    System.out.println("Pilihan Tidak Tersedia");
-            }
-        } while (pilihUtama != 0);   
+        // With inputs
+        // do {
+        //     System.out.println("Menu: ");
+        //     System.out.println("1. Tambah Data");
+        //     System.out.println("2. Tampilkan Data");
+        //     System.out.println("0. Keluar");
+        //     System.out.print("Pilih Menu: ");
+        //     pilihUtama = sc.nextInt();
+        //     switch (pilihUtama) {
+        //         case 1:
+        //             System.out.println("Tambah Data Mahasiswa:");
+        //             System.out.print("Nim: ");
+        //             String nim = sc.next();
+        //             System.out.print("Nama: ");
+        //             String nama = sc.next();
+        //             System.out.print("Kelas: ");
+        //             String kelas = sc.next();
+        //             System.out.print("IPK: ");
+        //             double ipk = sc.nextDouble();
+        //             Mahasiswa22 mhs = new Mahasiswa22(nim, nama, kelas, ipk);
+
+        //             menuTambahData();
+        //             pilihTambah = sc.nextInt();
+
+        //             switch (pilihTambah) {
+        //                 case 1:
+        //                     sll.addFirst(mhs);
+        //                     break;
+        //                 case 2:
+        //                     sll.addLast(mhs);
+        //                     break;
+        //                 case 3:
+        //                     System.out.print("Masukkan Nama Data yang Akan Dimasukkan: ");
+        //                     String key = sc.next();
+        //                     sll.insertAfter(key, mhs);
+        //                     break;
+        //                 case 4:
+        //                     System.out.print("Masukkan Index Data yang Akan Dimasukkan: ");
+        //                     int index = sc.nextInt();
+        //                     sll.insertAt(index, mhs);
+        //                     break;
+        //                 default:
+        //                     System.out.println("Pilihan Tidak Tersedia");
+        //                 }
+        //                 sll.print();
+        //             break;
+        //         case 2:
+        //             sll.print();
+        //             break;
+        //         case 0:
+        //             System.out.println("Keluar");
+        //             break;
+        //         default:
+        //             System.out.println("Pilihan Tidak Tersedia");
+        //     }
+        // } while (pilihUtama != 0);
+        
+        // Without inputs
+        // Adding data
+        sll.print();
+        sll.addFirst(mhs4);
+        sll.print();
+        sll.addLast(mhs1);
+        sll.print();
+        sll.insertAfter("Dirga", mhs3);
+        sll.insertAt(2, mhs2);
+        sll.print();
+        
+        // Removing data
+        System.out.println("Data index 1: ");
+        sll.getData(1);
+
+        System.out.println("Data mahasiswa a.n. Bimon berada pada index: " + sll.indexOf("Bimon"));
+        System.out.println();
+
+        sll.removeFirst();
+        sll.removeLast();
+        sll.print();
+        sll.removeAt(0);
+        sll.print();
         sc.close();
     }
 }
